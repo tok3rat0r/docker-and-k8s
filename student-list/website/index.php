483e37f846da1f916767f2250e4a389b7f1a2888
Owen Jones <owenmjones1@gmail.com>
2021-08-07 00:56:06 +0200

    Try to communicate between webapp and api

--- student-list/website/index.php
+++ student-list/website/index.php
@@ -25,8 +25,7 @@
                 "http" => array(
                 "header" => "Authorization: Basic " . base64_encode("$username:$password"),
               )));
-
-              $url = 'http://<api_ip_or_name:port>/pozos/api/v1.0/get_student_ages';
+              $url = 'pozos-api:5000/pozos/api/v1.0/get_student_ages';
               $list = json_decode(file_get_contents($url, false, $context), true);
               echo "<p style='color:red;; font-size: 20px;'>This is the list of the student with age</p>";
               foreach($list["student_ages"] as $key => $value) {

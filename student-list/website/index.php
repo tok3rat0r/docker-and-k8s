264b4eaef5f50e074d00999e5496e1db9b8bcd5b
Owen Jones <owenmjones1@gmail.com>
2021-08-07 18:25:21 +0200

    Correct docker-compose problem and successfully run app

--- student-list/website/index.php
+++ student-list/website/index.php
@@ -15,6 +15,15 @@
             </form>
 
             <?php
+            function curl_get_contents($url,$username,$password) {
+                $curlHandle = curl_init();
+                curl_setopt( $curlHandle , CURLOPT_URL, $url );
+                curl_setopt( $curlHandle , CURLOPT_RETURNTRANSFER, 1 );
+                curl_setopt( $curlHandle , CURLOPT_USERPWD, "$username:$password" );
+                $result = curl_exec( $curlHandle );
+                curl_close( $curlHandle );
+                return $result;
+            }
               if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
               {
               $username = getenv('USERNAME');
@@ -25,11 +34,17 @@
                 "http" => array(
                 "header" => "Authorization: Basic " . base64_encode("$username:$password"),
               )));
-              $url = 'http://localhost:5000/pozos/api/v1.0/get_student_ages';
-              $list = json_decode(file_get_contents($url, false, $context), true);
-              echo "<p style='color:red;; font-size: 20px;'>This is the list of the student with age</p>";
-              foreach($list["student_ages"] as $key => $value) {
-                  echo "- $key are $value years old <br>";
+              $url = 'pozos-api:5000/pozos/api/v1.0/get_student_ages';
+              $return_val = curl_get_contents($url, $username, $password);
+              if ($return_val === false) {
+                echo "<p style='color:red;; font-size: 20px;'>Unable to get student ages from API</p>";
+              }
+              else {
+                $list = json_decode($return_val, true);
+                echo "<p style='color:red;; font-size: 20px;'>This is the list of the student with age</p>";
+                foreach($list["student_ages"] as $key => $value) {
+                    echo "- $key are $value years old <br>";
+                }
               }
              }
             ?>
